2810b61f6a1984f145e9731ee2a6393b30df6c4c
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-03-29 09:40:22 +0100

    feat: echo

---
+++ ficha4_php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_Ficha4</title>
+</head>
+<body>
+    <h1>Olá Mundo!</h1>
+    <?php
+    $dia=29;
+    $mes=03;
+    $ano=2023;
+    echo 'Data de hoje =  ' . $dia . '/' . $mes . '/' . $ano;
+    ?>
+</body>
+</html>
