1e8a97fd121f0321b95e9c0f320b8cc403a2aae2
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-03-29 10:37:59 +0100

    feat: html

--- ficha4_php/index.php
+++ ficha4_php/index.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>PHP_Ficha4</title>
 </head>
 <body>
-    <img src="https://ead.ipleiria.pt/2022-23/pluginfile.php/166632/question/questiontext/693297/7/4353030/estg_h.png" alt="IPLeiria">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <div class="container">
-        <div class="row">
+        <div class="row align-items-center" >
+            <img src="https://ead.ipleiria.pt/2022-23/pluginfile.php/166632/question/questiontext/693297/7/4353030/estg_h.png" alt="estg">
             <form action="">
                 <label for="username">Username: </label><br>
-                <input type="text" id="username" name="username" value=""><br>
+                <input type="text" id="username" name="username" value="username" required><br>
                 <label for="password">Password</label><br>
-                <input type="text" id="password" name="passowrd" value=""><br><br>
+                <input type="text" id="password" name="password" value="password" required><br><br>
                 <input type="submit" value="Submit">
             </form>
         </div>
