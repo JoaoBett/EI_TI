b2072b2df96eb1531e95c8330cd946542df57184
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-04-26 16:16:37 +0100

    feat: arduino install

---
+++ arduino/ficha5-arduino/sketch_apr26b/sketch_apr26b.ino
@@ -0,0 +1,24 @@
+#include <WiFi101.h>
+#include <ArduinoHttpClient.h>
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+while(!Serial);
+
+char SSID[] = "labs";
+char PASS_WIFI[] = "robot1cA!ESTG"
+WiFi.begin(SSID,PASS_WIFI);
+WiFi.status();
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+while(Wifi.status()!=1){
+  Serial.println(.);
+  delay(500);
+}
+}
