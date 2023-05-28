b7eafeccf88a2c3ef83bca132ac78bbd9811b4b7
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-05-28 12:13:29 +0100

    feat: request 5seconds

--- ficha7_raspberryPi/pedidohttp.py
+++ ficha7_raspberryPi/pedidohttp.py
@@ -1,12 +1,15 @@
 #$ python -m pip install requests ---> this code makes the library work because it's not pre-installed with python
 import requests
+import time
 
-#Gets the current value of bitcoin
-r = requests.get("http://iot.dei.estg.ipleiria.pt/api/api.php?sensor=btc", auth=('user','pass'))
+while True:
+    #Gets the current value of bitcoin
+    r = requests.get("http://iot.dei.estg.ipleiria.pt/api/api.php?sensor=btc", auth=('user','pass'))
 
-
-
-if r.status_code == 200:
-    print(r.text)
-else:
-    print("Request failed with status code: " + r.status_code)
+    if r.status_code == 200:
+        print(time.strftime("%Y-%m-%d %H:%M:%S"), end="")
+        print(r.text)
+    else:
+        print("Request failed with status code: " + r.status_code)
+    
+    time.sleep(5)
