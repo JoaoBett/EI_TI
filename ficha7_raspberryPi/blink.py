eade8829116a959ef91eb8b43120f7b4da266ab1
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-05-28 12:39:47 +0100

    feat: led activation

---
+++ ficha7_raspberryPi/blink.py
@@ -0,0 +1,16 @@
+import time
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPI.GIO! You need to use 'sudo' to run the program.")
+
+GPIO.setmode(GPIO.BCM)
+
+out_channel = GPIO.setup(2, GPIO.OUT)
+
+while True:
+    #sets led to low
+    GPIO.output(out_channel,GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(out_channel,GPIO.HIGH)
